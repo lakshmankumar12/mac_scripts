70d1228c41cb51df9e9f4550b6633c4e9842e4bb
lakshman kumar <lakshmankumar@gmail.com>
2018-03-12 08:53:41 -0700

    parse-pandora-py updated

--- parse_pandora_info.py
+++ parse_pandora_info.py
@@ -7,11 +7,23 @@ import bs4
 
 ## Sample Input
 ##     <div class="Marquee Marquee--stopOnHover"><div class="Marquee__wrapper__content">Requiem For A Dream</div></div>`Jennifer Thomas`Illumination`2:22`3:34`Playing
+##  or
+##     <div class="Marquee Marquee--stopOnHover"><div class="Marquee__hiddenSizer">Apologize (Feat. OneRepublic)</div><div class="Marquee__wrapper"><div class="Marquee__wrapper__content--animating" style="animation-duration: 5s;"><div class="Marquee__wrapper__content__child">Apologize (Feat. OneRepublic)</div><div class="Marquee__wrapper__content__child">Apologize (Feat. OneRepublic)</div></div></div></div>
 
 for line in fileinput.input():   #this one will pick stdin or if some arg(s) is/are given, it will open that as a file!
     (title,artist,album,elapsed,total,playing) = line.split("`")
 
 title_bs4 = bs4.BeautifulSoup(title,'html.parser')
+extracted_title = title_bs4.findAll("div", {"class": "Marquee__wrapper__content"})
+if not extracted_title:
+    extracted_title = title_bs4.findAll("div", {"class": "Marquee__wrapper__content__child"})
+if not extracted_title:
+    # huh!
+    extracted_title = title_bs4('div')
+
+extracted_title = extracted_title[0].get_text()
+
+
 print ('''\
 Pandora Song Info
 -----------------
@@ -20,4 +32,5 @@ Artist:  {}
 Album:   {}
 Elapsed: {}
 Total:   {}
-Playing: {}'''.format(title_bs4('div')[0].get_text(), artist, album, elapsed, total, playing))
+Playing: {}'''.format(extracted_title, artist, album, elapsed, total, playing))
+
