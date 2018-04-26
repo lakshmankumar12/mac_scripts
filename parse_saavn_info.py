009f97426a1297fb0765f66cf7e80d137c46f5a7
lakshman kumar <lakshmankumar@gmail.com>
2018-04-26 15:26:09 -0700

    retry saavn

--- parse_saavn_info.py
+++ parse_saavn_info.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 
 from __future__ import print_function
 import fileinput
@@ -23,27 +23,34 @@ js = [
       mac_script_helper.SaveDocCmd,
      ]
 
-err,page,_ = bwsrTab.sendCommands(js)
-if err != 0:
-    print ("Trouble in getting page-info from saavn")
-    sys.exit(1)
+songInfo = None
+attempt = 1
+while attempt <= 3:
+    err,page,_ = bwsrTab.sendCommands(js)
+    if err != 0:
+        print ("Trouble in getting page-info from saavn")
+        sys.exit(1)
 
-pageSoup = bs4.BeautifulSoup(page, 'html.parser')
-metainfo = pageSoup.findAll("div", {"class": "meta-info"})
+    pageSoup = bs4.BeautifulSoup(page, 'html.parser')
+    metainfo = pageSoup.findAll("div", {"class": "meta-info"})
 
-with open ('/tmp/a.html','w') as fd:
-    fd.write(pageSoup.prettify())
+    with open ('/tmp/a.html','w') as fd:
+        fd.write(pageSoup.prettify())
 
-if not metainfo:
-    print ("Trouble in getting meta-info from page")
-    sys.exit(1)
-metainfo = metainfo[0]
-songInfo = metainfo.findAll("div", {"class": "song-json"})
-if not songInfo:
-    print ("Trouble in getting song-json from page")
-    sys.exit(1)
-songInfo_jsonStr = songInfo[0].get_text()
-songInfo = json.loads(songInfo_jsonStr)
+    if not metainfo:
+        print ("Trouble in getting meta-info from page")
+        sys.exit(1)
+    metainfo = metainfo[0]
+    songInfo = metainfo.findAll("div", {"class": "song-json"})
+    if not songInfo:
+        print ("Trouble in getting song-json from page .. attempt:{}".format(attempt))
+        if (attempt <= 3):
+            attempt += 1
+            continue
+        sys.exit(1)
+    songInfo_jsonStr = songInfo[0].get_text()
+    songInfo = json.loads(songInfo_jsonStr)
+    break;
 
 elapsedSoup = pageSoup.find('span', {"id": "track-elapsed"})
 if not elapsedSoup:
